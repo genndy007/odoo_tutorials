84d9a3d8255fabd544c14bc691eeec5ff1eb8426
Hennadii Kochev <hennadii.kochev@ronis.info>
2024-05-29 01:59:27 +0300

    manifest create module

---
+++ real_estate/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Real Estate App',
+    'version': '0.1.0',
+    'category': 'Tutorials/RealEstateApp',
+    'summary': 'Kochev Hennadii Tutorialed Real Estate App',
+    'description': '29 May 2024 created tutorialed Estate App',
+    'author': 'Hennadii Kochev',
+    'website': 'https://github.com/genndy007',
+
+    'application': True,
+    'installable': True,
+    'depends': [
+        'base',
+    ],
+}
